92ffc65a26a8f5200633c447b154ea8afbe3210d
Damar Khoirul Huda <damar.huda@javan.co.id>
2021-12-22 13:07:31 +0700

    lala

--- src/App.js
+++ src/App.js
@@ -1,31 +1,43 @@
-import React, { useState } from 'react';
+import React, {useState, useEffect} from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
+import AuthContect from "./store/auth-contect";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
-    setIsLoggedIn(true);
-  };
+    useEffect(() => {
+        const stored = localStorage.getItem('isLoggedIn');
+        if (stored === '1') {
+            setIsLoggedIn(true);
+        }
+    }, []);
 
-  const logoutHandler = () => {
-    setIsLoggedIn(false);
-  };
+    const loginHandler = (email, password) => {
+        // We should of course check email and password
+        // But it's just a dummy/ demo anyways
+        localStorage.setItem('isLoggedIn', '1');
+        setIsLoggedIn(true);
+    };
 
-  return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
-      <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
-      </main>
-    </React.Fragment>
-  );
+    const logoutHandler = () => {
+        setIsLoggedIn(false);
+    };
+
+    return (
+        <AuthContect.Provider value={{
+            isLoggedIn: isLoggedIn,
+            onLogout: logoutHandler,
+        }}>
+            <MainHeader />
+            <main>
+                {!isLoggedIn && <Login onLogin={loginHandler}/>}
+                {isLoggedIn && <Home onLogout={logoutHandler}/>}
+            </main>
+        </AuthContect.Provider>
+    );
 }
 
 export default App;
--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,29 +1,25 @@
-import React from 'react';
+import React, {useContext} from 'react';
 
 import classes from './Navigation.module.css';
+import AuthContect from "../../store/auth-contect";
 
-const Navigation = (props) => {
-  return (
-    <nav className={classes.nav}>
-      <ul>
-        {props.isLoggedIn && (
-          <li>
-            <a href="/">Users</a>
-          </li>
-        )}
-        {props.isLoggedIn && (
-          <li>
-            <a href="/">Admin</a>
-          </li>
-        )}
-        {props.isLoggedIn && (
-          <li>
-            <button onClick={props.onLogout}>Logout</button>
-          </li>
-        )}
-      </ul>
-    </nav>
-  );
+const Navigation = () => {
+    const ctx = useContext(AuthContect);
+    return (
+        <nav className={classes.nav}>
+            <ul>
+                {ctx.isLoggedIn && (<li>
+                    <a href="/">Users</a>
+                </li>)}
+                {ctx.isLoggedIn && (<li>
+                    <a href="/">Admin</a>
+                </li>)}
+                {ctx.isLoggedIn && (<li>
+                    <button onClick={ctx.onLogout}>Logout</button>
+                </li>)}
+            </ul>
+        </nav>
+    );
 };
 
 export default Navigation;
